5adcd4ccc10a702d994069580b844bbb2f40704f
TBKT-SYSTEM <117958477+TBKT-SYSTEM@users.noreply.github.com>
2023-11-07 08:53:16 +0700

    update_test

--- QGate_system/QGate_system/qgateLogin.Designer.cs
+++ QGate_system/QGate_system/qgateLogin.Designer.cs
@@ -40,7 +40,7 @@ namespace QGate_system
             this.tbLoginUser.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.tbLoginUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tbLoginUser.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(45)))), ((int)(((byte)(66)))));
-            this.tbLoginUser.Location = new System.Drawing.Point(128, 379);
+            this.tbLoginUser.Location = new System.Drawing.Point(169, 347);
             this.tbLoginUser.Name = "tbLoginUser";
             this.tbLoginUser.Size = new System.Drawing.Size(284, 48);
             this.tbLoginUser.TabIndex = 0;
@@ -60,7 +60,7 @@ namespace QGate_system
             // 
             this.lbExitApp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.lbExitApp.BackColor = System.Drawing.Color.Transparent;
-            this.lbExitApp.Location = new System.Drawing.Point(634, 518);
+            this.lbExitApp.Location = new System.Drawing.Point(634, 509);
             this.lbExitApp.Name = "lbExitApp";
             this.lbExitApp.Size = new System.Drawing.Size(154, 73);
             this.lbExitApp.TabIndex = 2;
